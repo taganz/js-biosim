d3346bbe61cc3d6d59637df64fe34e494b1b950e
Ricard Dalmau <rdalmau@gmail.com>
2024-02-25 12:49:31 +0100

    comment

--- src/creature/selection/InsideReproductionAreaSelection.ts
+++ src/creature/selection/InsideReproductionAreaSelection.ts
@@ -2,6 +2,7 @@ import World from "../../world/World";
 import Creature from "../Creature";
 import SelectionMethod from "./SelectionMethod";
 
+// return all creatures alive and in a area of type 0
 export default class InsideReproductionAreaSelection
   implements SelectionMethod
 {
@@ -13,7 +14,7 @@ export default class InsideReproductionAreaSelection
 
       if (
         creature.isAlive &&
-        gridPoint.areas.find((area) => area.areaType === 0)
+        gridPoint.areas.find((area) => area.areaType === 0)  // there is an area of type 0 at creature position
       ) {
         parents.push(creature);
       }
