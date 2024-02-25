5f01878deaa77ecc636c45d2b3f8bfa3221848b9
Ricard Dalmau <rdalmau@gmail.com>
2024-02-25 20:53:43 +0100

    organized map config in "sims" and added zoned population strategy

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "./styles/style.scss";
 import AsexualRandomPopulation from "./creature/population/AsexualRandomPopulation";
+import AsexualZonePopulation from "./creature/population/AsexualZonePopulation";
 import World from "./world/World";
 import { MutationMode } from "./creature/genome/MutationMode";
 import WebUI from "./web/WebUI";
@@ -13,11 +14,93 @@ const world = new World(
   100
 );
 
+
+// Default values
+world.initialPopulation = 1000;
+
+world.initialGenomeSize = 4;
+world.maxGenomeSize = 30;
+world.maxNumberNeurons = 15;
+world.timePerStep = 0;
+world.stepsPerGen = 300;
+world.immediateSteps = 1;
+world.mutationProbability = 0.05;
+world.geneInsertionDeletionProbability = 0.015;
+world.deletionRatio = 0.5;
+world.mutationMode = MutationMode.wholeGene;
+world.pauseBetweenGenerations = 0;
+
 // Environment
-const populationStrategy = new AsexualRandomPopulation();
+//const populationStrategy = new AsexualRandomPopulation();
 // const selectionMethod = new EastWallSelection();
 // const selectionMethod = new CenterRectangleSelection(0.5, 0.5);
+//const selectionMethod = new InsideReproductionAreaSelection();
+
+// SIMULATION ENVIRONMENT CONFIGURATIONS
+
+// SIM1 
+// 24/2/24
+/*
+const populationStrategy = new AsexualRandomPopulation();
 const selectionMethod = new InsideReproductionAreaSelection();
+world.obstacles = [
+    new RectangleObject(world, 0, 0.3, 0.7, 0.1),
+    new RectangleObject(world, 0.8, 0.8, 0.2, 0.2),
+  ];
+  world.areas = [
+    new RectangleReproductionArea(world, 0, 0, 0.35, 0.35, true),   // RD
+  ];
+*/
+
+// SIM2
+// 24/2/24
+/*
+const populationStrategy = new AsexualRandomPopulation();
+const selectionMethod = new InsideReproductionAreaSelection();
+world.obstacles = [
+  //  new RectangleObject(world, 0, 0, 0.2, 0.2),
+    new RectangleObject(world, 0.35, 0, 0.65, 0.25),    // barra vertical
+    new RectangleObject(world, 0, 0.4, 0.7, 0.1),     // barra horitzontal
+    new RectangleObject(world, 0.8, 0.8, 0.2, 0.2),
+  ];
+  world.areas = [
+    new RectangleReproductionArea(world, 0, 0, 0.35, 0.35, true),   // RD
+  ];
+*/
+
+/*
+// SIM3
+// 24/2/24
+const populationStrategy = new AsexualRandomPopulation();
+const selectionMethod = new InsideReproductionAreaSelection();
+world.obstacles = [
+  //  new RectangleObject(world, 0, 0, 0.2, 0.2),
+    new RectangleObject(world, 0.35, 0, 0.65, 0.25),    // barra vertical
+    new RectangleObject(world, 0, 0.4, 0.7, 0.6),     // barra horitzontal
+  ];
+  world.areas = [
+    new RectangleReproductionArea(world, 0, 0, 0.35, 0.35, true),   // RD
+  ];
+*/
+
+
+// SIM4
+// 24/2/24
+//const populationStrategy = new AsexualRandomPopulation();
+const populationStrategy = new AsexualZonePopulation(); 
+world.xrPopulate = 0.8;
+world.yrPopulate = 0.6;
+world.initialPopulation = 300;
+const selectionMethod = new InsideReproductionAreaSelection();
+world.obstacles = [
+  //  new RectangleObject(world, 0, 0, 0.2, 0.2),
+    new RectangleObject(world, 0.35, 0, 0.65, 0.25),    // barra vertical
+    new RectangleObject(world, 0, 0.4, 0.7, 0.6),     // barra horitzontal
+  ];
+  world.areas = [
+    new RectangleReproductionArea(world, 0, 0, 0.35, 0.35, true),   // RD
+  ];
+
 
 // world.obstacles = [new RectangleObject(world, 0.15, 0.25, 0.1, 0.5)];
 
@@ -28,27 +111,15 @@ const selectionMethod = new InsideReproductionAreaSelection();
 //   new RectangleObject(world, 0.8, 0.8, 0.2, 0.2),
 // ];
 
-// A map divided in two sections by 5 squares and a reproduction zone in the center
-world.obstacles = [
-  new RectangleObject(world, 0, 0, 0.2, 0.2),
-  new RectangleObject(world, 0.2, 0.2, 0.2, 0.2),
-  new RectangleObject(world, 0.4, 0.4, 0.2, 0.2),
-  new RectangleObject(world, 0.6, 0.6, 0.2, 0.2),
-  new RectangleObject(world, 0.8, 0.8, 0.2, 0.2),
-];
-world.areas = [
-  new RectangleReproductionArea(world, 0.25, 0.25, 0.5, 0.5, true),
-];
-
 // world.areas = [
 //   new RectangleHealthArea(world, 0.4, 0.0, 0.2, 1, true, -1),
 //   new EllipseHealthArea(world, 0, 0.0, 0.3, 0.2, true, -1),
 // ];
 
-// world.areas = [
-//   new RectangleReproductionArea(world, 0.25, 0.25, 0.5, 0.5, true),
-//   new EllipseHealthArea(world, 0.375, 0.35, 0.25, 0.3, true, -1),
-// ];
+ //world.areas = [
+ //  new RectangleReproductionArea(world, 0.25, 0.25, 0.5, 0.5, true),
+ //  new EllipseHealthArea(world, 0.375, 0.35, 0.25, 0.3, true, -1),
+ //];
 
 // world.obstacles = [new RectangleObject(world, 0, 0, 0.6, 0.6)];
 // world.obstacles = [new RectangleObject(world, 0.15, 0.15, 0.5, 0.5)];
@@ -158,21 +229,10 @@ world.areas = [
 //   new RectangleObject(world, 0, 60, 100, 40, false),
 // ];
 
-// Default values
-world.initialPopulation = 1000;
+
+
 world.populationStrategy = populationStrategy;
 world.selectionMethod = selectionMethod;
-world.initialGenomeSize = 4;
-world.maxGenomeSize = 30;
-world.maxNumberNeurons = 15;
-world.timePerStep = 0;
-world.stepsPerGen = 300;
-world.immediateSteps = 1;
-world.mutationProbability = 0.05;
-world.geneInsertionDeletionProbability = 0.015;
-world.deletionRatio = 0.5;
-world.mutationMode = MutationMode.wholeGene;
-world.pauseBetweenGenerations = 0;
 
 // Create UI
 new WebUI(world);
